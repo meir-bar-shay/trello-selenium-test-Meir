c0f79217b40ce644cc4145e49996a2efd2a64690
meir-bar-shay <58541781+meir-bar-shay@users.noreply.github.com>
2019-12-24 13:08:25 +0200

    Test-Base

--- src/test/java/com/meir/trello/LoginTest.java
+++ src/test/java/com/meir/trello/LoginTest.java
@@ -1,30 +1,14 @@
 package com.meir.trello;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
+public class LoginTest extends TestBase{
 
-public class LoginTest {
-    WebDriver wd;
-
-    @BeforeClass
-    public void setUp(){
-        wd = new ChromeDriver();
-        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        wd.get("https://trello.com/");
-    }
     @Test
     public void testLogin() throws InterruptedException {
         //click login
         clickLogin();
-
-
-        //
-
     }
 
     public void clickLogin() throws InterruptedException {
@@ -32,8 +16,8 @@ public class LoginTest {
         wd.findElement(By.id("user")).click();
         wd.findElement(By.id("user")).clear();
         wd.findElement(By.id("user")).sendKeys("meir.bar.shay@gmail.com");
-Thread.sleep(20000);
-        if (isElementPresent(By.id("password"))){
+Thread.sleep(10000);
+        if (wd.findElement(By.id("password")).isDisplayed()){
             wd.findElement(By.id("password")).click();
             wd.findElement(By.id("password")).clear();
             wd.findElement(By.id("password")).sendKeys("Trello2019");
@@ -53,4 +37,6 @@ Thread.sleep(20000);
     public boolean isElementPresent(By locator){
         return wd.findElements(locator).size()>0;
     }
+
 }
+
---
+++ src/test/java/com/meir/trello/TestBase.java
@@ -0,0 +1,25 @@
+package com.meir.trello;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    WebDriver wd;
+
+    @BeforeClass
+    public void setUp(){
+        wd = new ChromeDriver();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.get("https://trello.com/");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        wd.quit();
+    }
+}
